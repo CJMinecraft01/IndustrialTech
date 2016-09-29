119edd4816089b73dfd07e72eb57c8d2fbe7e661
CJMinecraft01 <speed2pc2@gmail.com>
2016-09-29 17:56:05 +0100

    Forge Gradlew

---
+++ src/main/java/cjminecraft/industrialtech/Reference.java
@@ -0,0 +1,11 @@
+package cjminecraft.industrialtech;
+
+public class Reference {
+	
+	public static final String MODID = "industrialtech";
+	public static final String NAME = "Industrial Tech";
+	public static final String VERSION = "0.3.0";
+	public static final String CLIENT_PROXY_CLASS = "cjminecraft.industrialtech.proxy.ClientProxy";
+	public static final String SERVER_PROXY_CLASS = "cjminecraft.industrialtech.proxy.CommonProxy";
+
+}
