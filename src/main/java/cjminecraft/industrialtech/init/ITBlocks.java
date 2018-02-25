a7c6bfbcb315035a9c0e6f82b377d8ca28f7bb03
CJMinecraft01 <speed2pc2@gmail.com>
2018-02-25 13:49:44 +0000

    Make BlockPhotonGenarator just Block

--- src/main/java/cjminecraft/industrialtech/init/ITBlocks.java
+++ src/main/java/cjminecraft/industrialtech/init/ITBlocks.java
@@ -3,11 +3,12 @@ package cjminecraft.industrialtech.init;
 import cjminecraft.industrialtech.blocks.BlockPhotonGenerator;
 import cjminecraft.industrialtech.utils.registries.annotations.RegisterBlock;
 import cjminecraft.industrialtech.utils.registries.annotations.RegisterItem;
+import net.minecraft.block.Block;
 
 public class ITBlocks {
 
     @RegisterBlock(registryName = "photon_generator")
     @RegisterItem(registryName = "photon_generator")
-    public static BlockPhotonGenerator PHOTON_GENERATOR;
+    public static Block PHOTON_GENERATOR;
 
 }
